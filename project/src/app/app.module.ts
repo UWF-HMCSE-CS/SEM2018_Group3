4eefbb3bffd7f6beb5e57ec7017be410d915719b
Joshua Wilkes <jrw71@students.uwf.edu>
2018-10-02 22:31:57 -0500

    Added support for FontAwesome icons; login page; routing to user, professional, and new account components

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -2,10 +2,57 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { PatientComponent } from './patient/patient.component';
+import { ProfessionalComponent } from './professional/professional.component';
+import { NewAccountComponent } from './new-account/new-account.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  { path: 'newAccount', component: NewAccountComponent },
+  { path: 'user', component: PatientComponent },
+  { path: 'professional', component: ProfessionalComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  /*   { path: 'hero/:id', component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  {
+    path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent } */
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PatientComponent,
+    ProfessionalComponent,
+    NewAccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes),
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
